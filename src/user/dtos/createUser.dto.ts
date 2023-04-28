afbca1c389970e24ec013f380c092102c0b4f0f0
Pedro Silva <pedro.teles@igcp.com.br>
2023-04-28 15:26:51 -0300

    :sparkles: feat: (user) validation pipe and return dto object

--- src/user/dtos/createUser.dto.ts
+++ src/user/dtos/createUser.dto.ts
@@ -1,7 +1,19 @@
-export interface CreateUserDto {
+import { IsString } from "class-validator";
+
+export class CreateUserDto {
+  
+  @IsString()
   name: string;
+  
+  @IsString()
   email: string;
+  
+  @IsString()
   password: string;
+
+  @IsString()
   phone: string;
+  
+  @IsString()
   cpf: string;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,21 +1,25 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserService } from './user.service';
 import { UserEntity } from './entities/user.entity';
+import { ReturnUserDto } from './dtos/returnUser.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
   @Post()
+  @UsePipes(ValidationPipe)
   async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity> {
     // variable createUser will store the data sent by the user in the body of the request
     return this.userService.createUser(createUser)
   }
 
   @Get()
-  async getAllUsers(): Promise<UserEntity[]> {
-    return this.userService.getAllUsers();
+  async getAllUsers(): Promise<ReturnUserDto[]> {
+    return (await this.userService.getAllUsers()).map(
+      (UserEntity) => new ReturnUserDto(UserEntity),
+    )
   }
 
 }
