e894fd6294222b90cf5f83447ba93a157a72f451
Pedro Silva <pedro.teles@igcp.com.br>
2023-05-30 10:14:29 -0300

    :sparkles: feat: added unit tests to city and state

---
+++ src/state/__tests__/state.service.spec.ts
@@ -0,0 +1,47 @@
+import { Repository } from "typeorm";
+import { StateService } from "../state.service";
+import { StateEntity } from "../entities/state.entity";
+import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { stateMock } from "../__mocks__/state.mock";
+
+describe('StateService', () => {
+    let service: StateService;
+    let stateRepository: Repository<StateEntity>;
+  
+    beforeEach(async () => {
+      const module: TestingModule = await Test.createTestingModule({
+        providers: [
+          StateService,
+          {
+            provide: getRepositoryToken(StateEntity),
+            useValue: {
+              find: jest.fn().mockResolvedValue([stateMock]),
+            },
+          },
+        ],
+      }).compile();
+  
+      service = module.get<StateService>(StateService);
+      stateRepository = module.get<Repository<StateEntity>>(
+        getRepositoryToken(StateEntity),
+      );
+    });
+  
+    it('should be defined', () => {
+      expect(service).toBeDefined();
+      expect(stateRepository).toBeDefined();
+    });
+  
+    it('should return list of states', async () => {
+      const state = await service.getAllStates();
+  
+      expect(state).toEqual([stateMock]);
+    });
+  
+    it('should return error in exception', async () => {
+      jest.spyOn(stateRepository, 'find').mockRejectedValueOnce(new Error());
+  
+      expect(service.getAllStates()).rejects.toThrowError();
+    });
+  });
